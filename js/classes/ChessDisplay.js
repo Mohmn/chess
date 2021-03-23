d126a64d975230fd48e8884003ba1d64cee1cd5c
Mohmn <naqashmohmin1@gmail.com>
2021-03-23 11:31:31 +0530

    added some new functions. self explanatory

--- js/classes/ChessDisplay.js
+++ js/classes/ChessDisplay.js
@@ -4,7 +4,7 @@ export default class ChessDisplay {
     constructor(board, document) {
         this.ChessBoard = board
         this.Document = document
-
+        this.domBoard = document.getElementById('board')
     }
 
     even(number) {
@@ -13,7 +13,7 @@ export default class ChessDisplay {
 
     draw() {
 
-        const Board = this.Document.getElementById('board')
+
 
         for (let i = 0; i < this.ChessBoard.size; i++) {
             const row = this.Document.createElement('div')
@@ -28,10 +28,10 @@ export default class ChessDisplay {
                 else
                     cell.className = "white-cell cells"
 
-                if(this.ChessBoard.board[i][j]){
+                if (this.ChessBoard.board[i][j]) {
 
-                    
-                   const p = this.ChessBoard.pieceAt(i,j)
+
+                    const p = this.ChessBoard.pieceAt(i, j)
                     cell.innerText = p.representation
                     cell.style.color = p.color
                 }
@@ -39,18 +39,51 @@ export default class ChessDisplay {
                 row.appendChild(cell)
             }
 
-            Board.appendChild(row)
+            this.domBoard.appendChild(row)
         }
 
     }
 
+    getCell(row, col) {
+        return this.domBoard.children[row].children[col]
+    }
 
-    highlightCell(cell){
+    visulaizeCell(cell) {
+        const elem = document.createElement('div')
+        elem.classList.add('dot')
+        cell.appendChild(elem)
+    }
+
+    unvisulaizeCell(cell){
+        const child = cell.children[0]
+        cell.removeChild(child)
+    }
+    highlightCell(cell) {
         cell.classList.add('highlight-cell')
     }
 
-    unHighlightCell(cell){
+    unHighlightCell(cell) {
         cell.classList.remove('highlight-cell')
     }
+    // obj of array of arrays locations
+    showPath(locations) {
+        for (const loc in locations) {
+            locations[loc].map((pos) => pos.map((p) => {
 
+                const cell = this.getCell(p[0], p[1])
+                this.visulaizeCell(cell)
+            }))
+        }
+    }
+
+    removePath(locations){
+        for (const loc in locations) {
+            locations[loc].map((pos) => pos.map((p) => {
+
+                const cell = this.getCell(p[0], p[1])
+                this.unvisulaizeCell(cell)
+            }))
+        }
+    }
+    
 }
