4a941865101a645a6f71e72c6014758a30369bfc
Mohmn <naqashmohmin1@gmail.com>
2021-03-24 11:55:13 +0530

    aaded some logic

--- js/classes/Chess.js
+++ js/classes/Chess.js
@@ -2,75 +2,95 @@ import Player from "./Player.js"
 
 export default class Chess {
 
-    constructor(board,display){
+    constructor(board, display) {
         this.board = board
         this.display = display
-        this.whitePlayer = new Player('white',true)
-        this.blackPlayer = new Player('black',false)
+        this.whitePlayer = new Player('white', true)
+        this.blackPlayer = new Player('black', false)
         this.DisplayBoard = this.display.domBoard
     }
 
     mousePressed() {
- 
 
-            for(let i=0;i<this.DisplayBoard.children.length;i++){
-                for(let j=0;j<this.DisplayBoard.children[i].children.length;j++){
-                
-                    this.DisplayBoard.children[i].children[j].addEventListener('click',
-                            ()=>this.handleClick([i,j],event))
-                }
+
+        for (let i = 0; i < this.DisplayBoard.children.length; i++) {
+            for (let j = 0; j < this.DisplayBoard.children[i].children.length; j++) {
+
+                this.DisplayBoard.children[i].children[j].addEventListener('click',
+                    () => this.handleClick([i, j], event))
             }
+        }
     }
 
 
     // self invoking function used to make movablePos as clisure var
-handleClick = (function(){
-    
-    let player
-    let movablePos = null
-    let i=0;
-    
-        function handling(pos,e){
-            i+=1
-            console.log(i)
-            if(this.whitePlayer.turn){
+    handleClick = (function () {
+
+        let player
+        let movablePos = null
+        let pieceSelectedByPlayer = null
+
+        function handling(pos, e) {
+
+            if (this.whitePlayer.turn) {
                 player = this.whitePlayer
-            }else{
+            } else {
                 player = this.blackPlayer
             }
-            if(player.FirstTurn()){
+            // if the board is filled with some piece
+            if (this.board.isFilled(pos[0], pos[1]) && player.FirstTurn() ) {
 
-                player.savePiece([pos,e.target])
-                player.numTimesClicked += 1
+                // save x,y pos selected by player on first turn
+                player.savePiece([pos, e.target])
                 this.display.highlightCell(e.target)
 
-                if (this.board.isFilled(pos[0],pos[1])){
-                    const p = this.board.pieceAt(pos[0],pos[1])
-                    movablePos = p.availableMoves(pos[0],pos[1], this.board.board)
-                    this.display.showPath(movablePos)
-                }
-            }else{
-                console.log('swaping from player ' + player.color + player.pieceSelected,movablePos)
+                player.numTimesClicked += 1
+                pieceSelectedByPlayer = this.board.pieceAt(pos[0], pos[1])
+                movablePos = pieceSelectedByPlayer.availableMoves(pos[0], pos[1], this.board.board)
+                this.display.showPath(movablePos)
+                
+
+            } else {
+
+                // 2nd turn
+                // console.log('swaping from player ' + player.color + player.pieceSelected, movablePos)
                 // swap turn
-                this.display.unHighlightCell(player.pieceSelected[1])
-                if(movablePos){
-                    console.log("DDD",movablePos)
+                
+
+                if (movablePos && pieceSelectedByPlayer.canMoveto(pos[0],pos[1],movablePos)) {
                     this.display.removePath(movablePos)
+                    this.display.unHighlightCell(player.pieceSelected[1])
+                    this.movePiece(pieceSelectedByPlayer,pos)
                     movablePos = null
+                    pieceSelectedByPlayer = null
+                    player.numTimesClicked = 1
+                    this.swapTurn()
                 }
-                player.numTimesClicked = 1
-                this.swapTurn()
+
             }
         }
 
-    return handling
-})()
+        return handling
+    })()
+
+
+    movePiece(piece,pos){
+        // console.log(piece)
+        this.board.removePiece(piece.getRow(),piece.getCol())
+        // if the pos contains piece then remove it
+        this.board.removePiece(pos[0],pos[1])
+        piece.moveTo(pos[0],pos[1])
+        this.board.addPiece(piece)
+        // console.log(this.board,'b')
+        this.display.reDraw()
+    }
+    
+    swapTurn() {
+        this.whitePlayer.turn = !this.whitePlayer.turn
+    }
 
 
 
-swapTurn(){
-    this.whitePlayer.turn = !this.whitePlayer.turn
-}
 
 
 
@@ -85,10 +105,7 @@ swapTurn(){
 
 
 
-
-
-
-//     isInCheckMate(ChessBoard board, int player)
-// isInCheck(ChessBoard board, int player)
-// isInStalemate(ChessBoard board, int player)
+    //     isInCheckMate(ChessBoard board, int player)
+    // isInCheck(ChessBoard board, int player)
+    // isInStalemate(ChessBoard board, int player)
 }
