169ac435d0bdb7a77057c0e195396fe377f4664b
Mohmn <naqashmohmin1@gmail.com>
2021-03-24 17:07:04 +0530

    working well

--- js/classes/Pieces/Bishop.js
+++ js/classes/Pieces/Bishop.js
@@ -1,6 +1,6 @@
 import Piece from "./Piece.js"
 import {
-   traverse_diagonals
+    traverse_diagonals
 } from "./traverse.js"
 
 export default class Tank extends Piece {
@@ -21,14 +21,40 @@ export default class Tank extends Piece {
     // so binary search can be used
     // 
     canMoveto(row, col, movableMoves) {
-        pass
+        // leftUpperDiag, rightLowerDiag, leftLowerDiag, rightUpperDiag
+
+        const leftUpperDiag = movableMoves['diags'][0]
+        if (this.getRow() > row && this.getCol() > col)
+            return this.pos_in_arr(row, col, leftUpperDiag)
+
+        const rightLowerDiag = movableMoves['diags'][1]
+        if (row > this.getRow() && col > this.getCol())
+            return this.pos_in_arr(row, col, rightLowerDiag)
+
+        const leftLowerDiag = movableMoves['diags'][2]
+        if (this.getRow() < row && this.getCol() > col)
+            return this.pos_in_arr(row, col, leftLowerDiag)
+
+        return this.pos_in_arr(row, col, movableMoves['diags'][3])
+
     }
 
 
 
+    pos_in_arr(row, col, arr_to_see) {
+        console.log(row,col,arr_to_see,'bbb')
+        for (const arr of arr_to_see) {
+            if ((arr[0] === row) && (arr[1] === col)) {
+                return true
+            }
+        }
+
+        return false
+    }
+
     availableMoves(row, col, chessBoard) {
-        const diagonals = traverse_fiagonals(row, col, chessBoard, this.color)
-        
+        const diagonals = traverse_diagonals(row, col, chessBoard, this.color)
+
         const movable = {
             diags: diagonals
         }
