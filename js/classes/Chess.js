c08830032fd9378b8278d1b3e995590ec7a246e3
Mohmn <naqashmohmin1@gmail.com>
2021-04-02 17:19:39 +0530

    added comments and rewrotrthe whole isInchess function

--- js/classes/Chess.js
+++ js/classes/Chess.js
@@ -117,7 +117,9 @@ export default class Chess {
     // immediate corners pawn
     // another King
 
+
     isInCheck(ChessBoard, player) {
+
         let King = null
         for (let i = 0; i < ChessBoard.size; i++) {
             for (let j = 0; j < ChessBoard.size; j++) {
@@ -129,43 +131,79 @@ export default class Chess {
                 }
 
             }
+
             if (King)
                 break
         }
 
-        function checkForOppPieces(row,col,piecesToCheck){
+        // fuction to make iterator can loop both forwards as well as backwards
+        function* range(start, end, leap) {
+            if (leap > 0) {
+                for (let i = start; i < end; i += leap)
+                    yield i
+            } else {
+                for (let i = start; i > end; i += leap)
+                    yield i
+            }
+        }
 
-            if (!ChessBoard.opponentPlayer(row, col,player.color))
+        function checkForRowsColumns(arg1, arg2) {
+
+            let iterator, index1, index2, pos
+            // here figuring which argumet is of type iterator
+            // beacuse i might had to iterate over row or over column
+            if (typeof (arg1) === "object") {
+                iterator = arg1
+                index1 = 0, index2 = 1
+                pos = arg2
+            } else {
+                iterator = arg2
+                pos = arg1
+                index1 = 1, index2 = 0
+            }
+
+            for (let i of iterator) {
+
+                let arr = [0, 0]
+
+                arr[index1] = i
+                arr[index2] = pos
+
+                if (ChessBoard.isFilled(arr[0], arr[1])) {
+                    if ((checkForOppPieces(arr[0], arr[1], pieces) === "stop") || chessed)
+                        break
+
+                }
+
+            }
+
+        }
+
+
+        function checkForOppPieces(row, col, piecesToCheck) {
+
+            if (!ChessBoard.opponentPlayer(row, col, player.color))
                 return "stop"
 
             for (const piece of piecesToCheck) {
-                if (ChessBoard.pieceAt(row, col).constructor.name === piece)  {
+                if (ChessBoard.pieceAt(row, col).constructor.name === piece) {
                     chessed = true
                     return
                 }
             }
 
-           
-            // if non-thretining pieces come before threating pieces then return
-            if(!chessed){
+            // if non-thretening pieces come before threating pieces then return
+            if (!chessed)
                 return "stop"
-            }
 
-        
         }
+
+
         let chessed = false
         const topCol = [King.getRow() - 1, King.getCol()]
         let pieces = ["Tank", "Queen"]
-        if (topCol[0] > 0 ) {
-
-            for (let i = topCol[0]; i >= 0; i--) {
-                if (ChessBoard.isFilled(i, topCol[1])) {
-                    if ( (checkForOppPieces(i, topCol[1],pieces) === "stop") || chessed )
-                        break
-                }
-            }
-        }
-
+        if (topCol[0] >= 0)
+            checkForRowsColumns(range(topCol[0], -1, -1), topCol[1])
         if (chessed) {
             console.log('topCOl', pieces)
             return
@@ -173,17 +211,8 @@ export default class Chess {
 
         const lowCol = [King.getRow() + 1, King.getCol()]
 
-        if (lowCol[0] < ChessBoard.size) {
-
-            for (let i = topCol[0]; i < ChessBoard.size; i++) {
-                if (ChessBoard.isFilled(i, lowCol[1])) {
-
-                    if ( (checkForOppPieces(i, lowCol[1],pieces) === "stop") || chessed )
-                        break
-                }
-            }
-        }
-
+        if (lowCol[0] < ChessBoard.size)
+            checkForRowsColumns(range(lowCol[0], ChessBoard.size, 1), lowCol[1])
         if (chessed) {
             console.log('lowCOl', pieces)
             return
@@ -191,61 +220,47 @@ export default class Chess {
 
         const leftCol = [King.getRow(), King.getCol() - 1]
 
-        if (leftCol[1] > 0) {
-            for (let i = leftCol[1]; i >= 0; i--) {
-                if (ChessBoard.isFilled(leftCol[0], i)) {
-
-
-                if ( (checkForOppPieces(leftCol[0], i,pieces) === "stop") || chessed )
-                    break
-                    
-                }
-            }
-        }
-
+        if (leftCol[1] >= 0)
+            checkForRowsColumns(leftCol[0], range(leftCol[1], -1, -1))
         if (chessed) {
             console.log('leftCol', pieces)
             return
         }
+
         const rightCol = [King.getRow(), King.getCol() + 1]
-
-        if (rightCol[1] < ChessBoard.size) {
-            for (let i = rightCol[1]; i < ChessBoard.size; i++) {
-                
-                if (ChessBoard.isFilled(rightCol[0], i)) {
-
-                if ( (checkForOppPieces(rightCol[0], i,pieces) === "stop") || chessed )
-                        break
-                }
-            }
-        }
-
+        if (rightCol[1] < ChessBoard.size)
+            checkForRowsColumns(rightCol[0], range(rightCol[1], ChessBoard.size, 1))
         if (chessed) {
             console.log('rightCol', pieces)
             return
         }
 
-        const TopLeftDiag = [King.getRow()-1,King.getCol()-1]
-        pieces = ["Queen","Bishop"]
-        if(TopLeftDiag[0] > 0 && TopLeftDiag[1] > 0){
+        function checkFordiagonals(arr1, arr2) {
 
-            // if the immediate left corner is Pawn then dont need to look out further
-            if(ChessBoard.isFilled(TopLeftDiag[0],TopLeftDiag[1])){
-                if( (checkForOppPieces(TopLeftDiag[0],TopLeftDiag[1],['Pawn']) === "stop")  || chessed){
-                    if (chessed) {
-                        console.log('TopLeftPawn', 'pawn')
-                        return
-                    }
-                }
+            let i = 0,
+                j = 0,
+                done = true
 
-            }
-            for (let i = TopLeftDiag[0], j = TopLeftDiag[1]; i >= 0 && j >= 0; i--, j--){
-                if (ChessBoard.isFilled(i, j)) {
-                   
-                    if( (checkForOppPieces(i,j,pieces) === "stop")  || chessed)
+            while (done) {
+
+                if (ChessBoard.isFilled(arr1[i], arr2[j])) {
+                    if ((checkForOppPieces(arr1[i], arr2[j], pieces) === "stop") || chessed)
                         break
                 }
+                i++, j++
+                done = (i < arr1.length) && (j < arr2.length)
             }
+
+        }
+
+        pieces = ["Queen", "Bishop"]
+
+        const TopLeftDiag = [King.getRow() - 1, King.getCol() - 1]
+        if (TopLeftDiag[0] > 0 && TopLeftDiag[1] > 0) {
+
+            const arr1 = Array.from(range(TopLeftDiag[0], -1, -1))
+            const arr2 = Array.from(range(TopLeftDiag[1], -1, -1))
+            checkFordiagonals(arr1, arr2)
 
         }
         if (chessed) {
@@ -253,78 +268,37 @@ export default class Chess {
             return
         }
 
-        const lowerRightDiag = [King.getRow() + 1,King.getCol() + 1] 
-        if(lowerRightDiag[0] < ChessBoard.size && lowerRightDiag[1] < ChessBoard.size){
+        const lowerRightDiag = [King.getRow() + 1, King.getCol() + 1]
+        if (lowerRightDiag[0] < ChessBoard.size && lowerRightDiag[1] < ChessBoard.size) {
 
-            // if the immediate lower right corner is Pawn then dont need to look out further
-            if(ChessBoard.isFilled(lowerRightDiag[0],lowerRightDiag[1])){
-                if( checkForOppPieces(lowerRightDiag[0],lowerRightDiag[1],['Pawn']) || chessed){
-                    if (chessed) {
-                        console.log('lowerRightPawn', 'pawn')
-                        return
-                    }
-                }
-
-            }
-
-            for (let i = lowerRightDiag[0], j = lowerRightDiag[1]; i < ChessBoard.size && j < ChessBoard.size; i++, j++){
-                if (ChessBoard.isFilled(i, j)) {
-                    if( (checkForOppPieces(i,j,pieces) === "stop")  || chessed)
-                        break
-                }
-            }
+            const arr1 = Array.from(range(lowerRightDiag[0], ChessBoard.size, 1))
+            const arr2 = Array.from(range(lowerRightDiag[1], ChessBoard.size, 1))
+            checkFordiagonals(arr1, arr2)
         }
         if (chessed) {
             console.log('LowerRighDIag', pieces)
             return
         }
 
-        const LowerLeftDiag = [King.getRow()+1,King.getCol()-1]
-        if( LowerLeftDiag[0] < ChessBoard.size && LowerLeftDiag[1] > 0){
+        const LowerLeftDiag = [King.getRow() + 1, King.getCol() - 1]
+        if (LowerLeftDiag[0] < ChessBoard.size && LowerLeftDiag[1] > 0) {
 
-            // if the immediate lower left corner is Pawn then dont need to look out further
-            if(ChessBoard.isFilled(LowerLeftDiag[0],LowerLeftDiag[1])){
-                if( checkForOppPieces(LowerLeftDiag[0],LowerLeftDiag[1],['Pawn']) || chessed){
-                    if (chessed) {
-                        console.log('lowerLeftPawn', 'pawn')
-                        return
-                    }
-                }
+            const arr1 = Array.from(range(LowerLeftDiag[0], ChessBoard.size, 1))
+            const arr2 = Array.from(range(LowerLeftDiag[1], -1, -1))
+            checkFordiagonals(arr1, arr2)
 
-            }
-
-            for (let i = LowerLeftDiag[0], j = LowerLeftDiag[1]; i < ChessBoard.size && j >= 0; i++, j--){
-                if (ChessBoard.isFilled(i, j)) {
-                    if( (checkForOppPieces(i,j,pieces) === "stop")  || chessed)
-                        break
-                }
-            }
         }
         if (chessed) {
             console.log('LowerLeftDIag', pieces)
             return
         }
-        const UpperRightDiag = [King.getRow()-1,King.getCol()+1]
+        const UpperRightDiag = [King.getRow() - 1, King.getCol() + 1]
 
-        if( UpperRightDiag[0] > 0   && UpperRightDiag[1] < ChessBoard.size){
+        if (UpperRightDiag[0] > 0 && UpperRightDiag[1] < ChessBoard.size) {
 
-            // if the immediate lower left corner is Pawn then dont need to look out further
-            if(ChessBoard.isFilled(UpperRightDiag[0],UpperRightDiag[1])){
-                if( checkForOppPieces(UpperRightDiag[0],ChessBoard.size[1],['Pawn']) || chessed){
-                    if (chessed) {
-                        console.log('UpperRightPawn', 'pawn')
-                        return
-                    }
-                }
-
-            }
-
-            for (let i = UpperRightDiag[0], j = UpperRightDiag[1]; i >=0  && j < ChessBoard.size; i--, j++){
-                if (ChessBoard.isFilled(i, j)) {
-                    if( (checkForOppPieces(i,j,pieces) === "stop")  || chessed)
-                        break
-                }
-            }
+            const arr1 = Array.from(range(UpperRightDiag[0], -1, -1))
+            const arr2 = Array.from(range(UpperRightDiag[1], ChessBoard.size, 1))
+            checkFordiagonals(arr1, arr2)
         }
 
         if (chessed) {
@@ -332,27 +306,75 @@ export default class Chess {
             return
         }
 
+        //pawns 
+        // pawns  moving down
+        // upper corners
+        const row = King.getRow(),
+            col = King.getCol()
+        let upperCornerPawns = []
+        if (row > 0) {
+            if (col > 0)
+                upperCornerPawns.push([row - 1, col - 1]) //leftuppercorner
+            if (col < ChessBoard.size)
+                upperCornerPawns.push([row - 1, col + 1]) //rightuppercorner
 
-        // now for knight pieces
-        let l_shape_coord = [
-            [-2, +1],[-2, -1],[+2, +1],[+2, -1],[-1, +2],[-1, -2],[+1, +2],[+1, -2]
-        ]
+            for (const piece of upperCornerPawns) {
+                if (ChessBoard.isFilled(piece[0], piece[1]) && ChessBoard.pieceAt(piece[0], piece[1]).constructor.name === "Pawn") {
+                    // upper pawn can only eat if they move down and are opposite
+                    const pawn = ChessBoard.pieceAt(piece[0], piece[1])
+                    if (!pawn.moveUp && ChessBoard.opponentPlayer(piece[0], piece[1], King.color)) {
+                        console.log("lowermovingPawns", piece)
+                        return
+                    }
 
-        for(let i=0;i<l_shape_coord.length;i++){
-            let x = l_shape_coord[i][0] + King.getRow()
-            let y = l_shape_coord[i][1] + King.getCol()
-           if ( ( (x >= 0 ) && (x < ChessBoard.size) ) && ((y >= 0 ) && (y < ChessBoard.size)) ){
-            if(ChessBoard.isFilled(x,y)){
-                checkForOppPieces(x,y,["Knight"])
-                if(chessed){
-                    console.log(ChessBoard.pieceAt(x,y),"Kniight xed")
-                    break
                 }
             }
-           }
         }
 
+
+        // pawnns moving up 
+        // lower cornerns
+        let lowerCornerPawns = []
+        if (row < ChessBoard.size) {
+            if (col > 0)
+                lowerCornerPawns.push([row + 1, col - 1]) //leftlowercorner
+            if (col < ChessBoard.size)
+                lowerCornerPawns.push([row + 1, col + 1]) //leftlowercorner
+
+            for (const piece of lowerCornerPawns) {
+                if (ChessBoard.isFilled(piece[0], piece[1]) && ChessBoard.pieceAt(piece[0], piece[1]).constructor.name === "Pawn") {
+                    // upper pawn can only eat if they move down and are opposite
+                    const pawn = ChessBoard.pieceAt(piece[0], piece[1])
+                    console.log(pawn.moveUp && ChessBoard.opponentPlayer(piece[0], piece[1], King.color))
+                    if (pawn.moveUp && ChessBoard.opponentPlayer(piece[0], piece[1], King.color)) {
+                        console.log("uppermovingPawns", piece)
+                        return
+                    }
+
+                }
+            }
+        }
+        // now for knight pieces
+        let l_shape_coord = [[-2, +1],[-2, -1],[+2, +1],[+2, -1],[-1, +2],[-1, -2],[+1, +2],[+1, -2]]
+
+        for (let i = 0; i < l_shape_coord.length; i++) {
+            let x = l_shape_coord[i][0] + King.getRow()
+            let y = l_shape_coord[i][1] + King.getCol()
+            if (((x >= 0) && (x < ChessBoard.size)) && ((y >= 0) && (y < ChessBoard.size))) {
+                if (ChessBoard.isFilled(x, y)) {
+                    checkForOppPieces(x, y, ["Knight"])
+                    if (chessed) {
+                        console.log(ChessBoard.pieceAt(x, y), "Kniight xed")
+                        break
+                    }
+                }
+            }
+        }
+
+        // now the peice left is oppking
+
     }
+
 
 
 
